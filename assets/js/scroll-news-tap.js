c6886c458f6412c96ea8ff75da380ab532d9bd84
xuanson2003 <doxuanson12b2003@gmail.com>
2024-10-04 15:15:45 +0700

    responsive to 1200px

---
+++ assets/js/scroll-news-tap.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    // news tap
+    const newsTabs = document.querySelector('.news-tabs');
+
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    newsTabs.addEventListener('mousedown', (e) => {
+        isDown = true;
+        newsTabs.classList.add('active');
+        startX = e.pageX - newsTabs.offsetLeft;
+        scrollLeft = newsTabs.scrollLeft;
+    });
+
+    newsTabs.addEventListener('mouseleave', () => {
+        isDown = false;
+        newsTabs.classList.remove('active');
+    });
+
+    newsTabs.addEventListener('mouseup', () => {
+        isDown = false;
+        newsTabs.classList.remove('active');
+    });
+
+    newsTabs.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - newsTabs.offsetLeft;
+        const walk = (x - startX) * 2; // Tốc độ kéo
+        newsTabs.scrollLeft = scrollLeft - walk;
+    });
+})
