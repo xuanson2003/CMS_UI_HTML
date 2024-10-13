9f268bc397ce951a35a3b6bad437cf345db66b8e
xuanson2003 <doxuanson12b2003@gmail.com>
2024-10-13 17:59:38 +0700

    update responsive home page

--- assets/js/nav-bar.js
+++ assets/js/nav-bar.js
@@ -19,15 +19,29 @@ $(document).ready(function () {
         const svg = document.getElementById('search-svg').querySelector('path');
     });
 
-    $('.nav-item').hover(function() {
-        $("<style>")
-            .prop("type", "text/css")
-            .html(".nav-item:hover::after { width: 100%; }")
-            .appendTo("head");
-    }, function() {
-        $("<style>")
-            .prop("type", "text/css")
-            .html(".nav-item::after { width: 0%; }")
-            .appendTo("head");
+    $('.nav-item').hover(
+        function () {
+            $('<style>').prop('type', 'text/css').html('.nav-item:hover::after { width: 100%; }').appendTo('head');
+        },
+        function () {
+            $('<style>').prop('type', 'text/css').html('.nav-item::after { width: 0%; }').appendTo('head');
+        }
+    );
+
+    // Lấy các phần tử cần thiết
+    const openMenuBtn = document.querySelector('.nav-bar-open-menu');
+    const closeMenuBtn = document.querySelector('.nav-bar-close-menu');
+    const navBarMain = document.querySelector('.nav-bar-main');
+
+    openMenuBtn.addEventListener('click', () => {
+        navBarMain.classList.add('active');
+        openMenuBtn.classList.remove('active'); 
+        closeMenuBtn.classList.add('active');
+    });
+
+    closeMenuBtn.addEventListener('click', () => {
+        navBarMain.classList.remove('active');
+        closeMenuBtn.classList.remove('active'); 
+        openMenuBtn.classList.add('active'); 
     });
 });
--- assets/js/scroll-news-tap.js
+++ assets/js/scroll-news-tap.js
@@ -1,33 +1,33 @@
-$(document).ready(function() {
-    // news tap
-    const newsTabs = document.querySelector('.news-tabs');
+// $(document).ready(function() {
+//     // news tap
+//     const newsTabs = document.querySelector('.news-tabs-box');
 
-    let isDown = false;
-    let startX;
-    let scrollLeft;
+//     let isDown = false;
+//     let startX;
+//     let scrollLeft;
 
-    newsTabs.addEventListener('mousedown', (e) => {
-        isDown = true;
-        newsTabs.classList.add('active');
-        startX = e.pageX - newsTabs.offsetLeft;
-        scrollLeft = newsTabs.scrollLeft;
-    });
+//     newsTabs.addEventListener('mousedown', (e) => {
+//         isDown = true;
+//         newsTabs.classList.add('active');
+//         startX = e.pageX - newsTabs.offsetLeft;
+//         scrollLeft = newsTabs.scrollLeft;
+//     });
 
-    newsTabs.addEventListener('mouseleave', () => {
-        isDown = false;
-        newsTabs.classList.remove('active');
-    });
+//     newsTabs.addEventListener('mouseleave', () => {
+//         isDown = false;
+//         newsTabs.classList.remove('active');
+//     });
 
-    newsTabs.addEventListener('mouseup', () => {
-        isDown = false;
-        newsTabs.classList.remove('active');
-    });
+//     newsTabs.addEventListener('mouseup', () => {
+//         isDown = false;
+//         newsTabs.classList.remove('active');
+//     });
 
-    newsTabs.addEventListener('mousemove', (e) => {
-        if (!isDown) return;
-        e.preventDefault();
-        const x = e.pageX - newsTabs.offsetLeft;
-        const walk = (x - startX) * 2; // Tốc độ kéo
-        newsTabs.scrollLeft = scrollLeft - walk;
-    });
-})
+//     newsTabs.addEventListener('mousemove', (e) => {
+//         if (!isDown) return;
+//         e.preventDefault();
+//         const x = e.pageX - newsTabs.offsetLeft;
+//         const walk = (x - startX) * 2; // Tốc độ kéo
+//         newsTabs.scrollLeft = scrollLeft - walk;
+//     });
+// })
